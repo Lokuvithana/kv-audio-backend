31d91280b2ff34384d4475aab7cf66870e34c2e3
Nisith Lokuvithana <nlokuvithana71@gmail.com>
2025-02-04 09:50:58 -0800

    added newfeatures such as deleteReview,updateReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -46,3 +46,85 @@ export function getReview(req,res){
 
     }
 }
+
+export function deleteReview(req,res){
+
+    const email = req.params.email; //request eke parameter ekaka enawanm eka allaganwa
+
+    if (req.user == null){
+        res.status(401).json({
+            message : "Please login and try again"
+        })
+        return
+    }
+
+    if (req.user.role == "admin"){
+
+        Review.deleteOne({email : email}).then(()=>{
+            res.json({
+                message : "Deletion sucessfully"
+            })
+        }).catch(()=>{
+            res.status(500).json({
+                message : "Deletion failed"
+            })
+        })
+        return
+
+    }else {
+        if(req.user.email == email){
+            Review.deleteOne({email : email}).then(()=>{
+                res.json({
+                    message : "Deletion successfully"
+                })
+            }).catch(()=>{
+                res.status(500).json({
+                    message : "deletion Failed"
+                })
+            })
+        }else{
+            res.status(403).json({
+                message : "You are not autherized to do it"
+            })
+        }
+    }
+    
+}
+
+export function approvedReview(req,res){
+    
+    const email = req.params.email;
+
+    if(req.user == null){
+        res.status(401).json({
+            message : "Please login and try again"
+        })
+        return
+    }
+
+    if(req.user.role == "admin"){
+        Review.updateOne({
+
+            email : email //methana mulin email eka araganwa
+        },
+        {
+            isApproved : true //itapsse eka approved karanwa JSON dekak wena wenama liyanwa ekata
+
+        }).then(()=>{
+            res.json({
+                message : "Review approved successfully"
+            })
+        }).catch(()=>{
+            res.status(500).json({
+                message : "Review approval failed"
+            })
+        })
+    }else{
+        res.status(403).json({
+            message : "You are not autherized to do it"
+        })
+        return
+    }
+
+
+}
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,10 +1,14 @@
 import express from "express"
-import { addReview, getReview } from "../controllers/reviewController.js";
+import { addReview, approvedReview, deleteReview, getReview } from "../controllers/reviewController.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.post("/" , addReview);
 
 reviewRouter.get("/" ,getReview);
+
+reviewRouter.delete("/:email",deleteReview); //we can passed the value as a parameter eth methana meke awlak thiyenawa mkd hithanna api /approved kiyala path ekaka haduwoth api dan uurl rkr para ekak passwkoroth u enne approved path ekata neme menna me /:email ekata anna e nisa me wage route api yatatama dala thiyagana oni
+
+reviewRouter.put("/approve/:email",approvedReview);
 
 export default reviewRouter;
